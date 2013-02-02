bb920086a98aae13e36e776f321852644eff6fcb
Wildan Maulana <wildan.m@gmail.com>
2013-02-03 01:28:42 +0700

    Membuat ERR untuk modul menu, dan skeleton modul retensi

---
+++ apps/qubit/modules/retention/actions/actions.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * retention actions.
+ *
+ * @package    example
+ * @subpackage retention
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class retentionActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    //$this->forward('default', 'module');
+  }
+
+  public function executeBrowse(sfWebRequest $request)
+  {
+  }  
+}
